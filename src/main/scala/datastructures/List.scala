78370b7cfc711e2b038710facb169a2ae355e555
ntk01 <n.taku.law@gmail.com>
2021-10-23 02:42:35 +0900

    drop, dropWhile and init

--- src/main/scala/datastructures/List.scala
+++ src/main/scala/datastructures/List.scala
@@ -1,5 +1,7 @@
 package datastructures
 
+import scala.annotation.tailrec
+
 sealed trait List[+A]
 case object Nil extends List[Nothing]
 
@@ -47,5 +49,31 @@ object List extends App {
     }
 
   println(setHead(List(1, 2, 3), 4))
+
+  @tailrec
+  def drop[A](l: List[A], n: Int): List[A] =
+    if (n <= 0) l
+    else l match {
+      case Nil => Nil
+      case Cons(_, xs) => drop(xs, n - 1)
+    }
+
+  println(drop(List(1, 2, 3, 4, 5), 2))
+
+  @tailrec
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] =
+    l match {
+      case Cons(h, t) if f(h) => dropWhile(t, f)
+      case _ => l
+    }
+
+  def init[A](l: List[A]): List[A] =
+    l match {
+      case Nil => sys.error("init on empty list")
+      case Cons(_, Nil) => Nil
+      case Cons(h, t) => Cons(h, init(t))
+    }
+
+  println(init(List(1, 2, 3)))
 
 }
