2676e181be6ceaa1ada08efdfe532bc72478ea60
ntk01 <n.taku.law@gmail.com>
2022-05-20 19:16:56 +0900

    add laziness

--- src/main/scala/errorhandling/Option.scala
+++ src/main/scala/errorhandling/Option.scala
@@ -3,18 +3,27 @@ package errorhandling
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
 
-object Main extends App {
+object Option extends App {
   println(Some(2).map(_ + 1))
+
+  def mean(xs: Seq[Double]): Option[Double] =
+    if (xs.isEmpty) None
+    else Some(xs.sum / xs.length)
+
+  def variance(xs: Seq[Double]): Option[Double] =
+    mean(xs).flatMap(m => mean(xs.map(x => math.pow(x - m, 2))))
+
+  println(variance(List(1.0, 2.0, 3.0)))
 }
 
 sealed trait Option[+A] {
   def map[B](f: A => B): Option[B] = this match {
-    case None => None
+    case None    => None
     case Some(a) => Some(f(a))
   }
 
   def getOrElse[B >: A](default: => B): B = this match {
-    case None => default
+    case None    => default
     case Some(a) => a
   }
 
@@ -25,10 +34,8 @@ sealed trait Option[+A] {
     this.map(Some(_)).getOrElse(ob)
 
   def filter(f: A => Boolean): Option[A] = this match {
-    // elseなしでも動く
     case Some(a) if f(a) => this
-//    case Some(a) => if (f(a)) this else None
-    case _ => None
+    case _               => None
   }
 
 }
--- src/main/scala/laziness/Stream.scala
+++ src/main/scala/laziness/Stream.scala
@@ -1,5 +1,25 @@
 package laziness
 
-object Stream {
+trait Stream[+A]
+case object Empty extends Stream[Nothing]
+case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
 
+object Stream extends App {
+  def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+    lazy val head = hd
+    lazy val tail = tl
+    Cons(() => head, () => tail)
+  }
+
+  def maybeTwice(b: Boolean, i: => Int) = if (b) i + i else 0
+  val x = maybeTwice(true, { println("hi"); 1 + 41 })
+  println(x)
+
+  def maybeTwice2(b: Boolean, i: => Int) = {
+    val j = i
+//    lazy val j = i
+    if (b) j + j + j else 0
+  }
+  val x2 = maybeTwice2(true, { println("hi"); 1 + 41 })
+  println(x2)
 }
